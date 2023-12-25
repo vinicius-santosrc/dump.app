c0983d35d87896a71046774c8acbcd0fc48d88a4
Vinícius Santos <viniciusssantos.pa@gmail.com>
2023-12-24 22:55:38 -0300

    add: Loading Content

---
+++ project/src/components/pages/feed/LoadingContentWhite.js
@@ -0,0 +1,18 @@
+import { Momentum, Ring } from '@uiball/loaders'
+
+
+export default function LoadingContentWhite() {
+    return (
+        <div className="loading-posts-dump-in-bottom-white loading-wrapper-bottom">
+            <Ring
+                size={40}
+                lineWeight={5}
+                speed={2}
+                color="white"
+            />
+        </div>
+
+
+    )
+}
+
